d5003ce7add26c1e62c3e4c6def028cd0c8b1160
alpernai <148302974+alpernai@users.noreply.github.com>
2024-08-30 17:36:11 +0000

    random card generator entrega

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let randomNumber = generateRandomNumber();
+  let randomSuit = generateRandomSuit();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".number").innerHTML = randomNumber;
+  document.querySelector("#top-suit").innerHTML = randomSuit;
+  document.querySelector("#bottom-suit").innerHTML = randomSuit;
+
+  if (randomSuit === "♦" || randomSuit === "♥") {
+    document.querySelector("#top-suit").classList.add("red");
+    document.querySelector("#bottom-suit").classList.add("red");
+  }
 };
+
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+}
+
+function generateRandomSuit() {
+  let suits = ["♣", "♠", "♦", "♥"];
+  let indexSuits = Math.floor(Math.random() * suits.length);
+  return suits[indexSuits];
+}
